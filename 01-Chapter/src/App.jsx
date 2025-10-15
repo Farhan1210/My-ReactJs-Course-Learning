da02aa5d9ebb5ff867553be369e666cccb573971
Farhan1210 <farhankhan12102004@gmail.com>
2025-10-15 18:48:00 +0500

    01-Chapter Done

---
+++ 01-Chapter/src/App.jsx
@@ -0,0 +1,13 @@
+function app() {
+  return (
+    <>
+      <div id="walid">
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+      </div>
+      <div id="chacha"></div>
+    </>
+  );
+}
+
+export default app;
